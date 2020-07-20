0dac1c011601daaaa84634160d409d9c72163593
RgMellon <renan_gian@hotmail.com>
2020-07-20 00:24:56 -0300

    add href

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -114,9 +114,13 @@ const Project: React.FC<RouteComponentProps<RouteProp>> = ({ match }) => {
                 project.imagesPreview.map((preview, index) => {
                   return (
                     <Slide index={index} key={preview}>
-                      {/* <a href={preview} rel="noreferrer" target="_blank"> */}
-                      <img src={preview} alt={preview} />
-                      {/* </a> */}
+                      <a
+                        href={preview}
+                        rel="noreferrer noopener"
+                        target="_blank"
+                      >
+                        <img src={preview} alt={preview} />
+                      </a>
                     </Slide>
                   );
                 })}
